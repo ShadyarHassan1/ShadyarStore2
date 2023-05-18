f77d582a32bf83a74b5fd7be278113af2f9fd164
ShadyarHassan1 <133672400+ShadyarHassan1@users.noreply.github.com>
2023-05-18 20:11:39 +0300

    Add files via upload

---
+++ pages/appleid.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>AppleID</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+ <a href="#" class="link back">
+          <i class="white-color f7-icons">arrow_left</i> Back  </a>
+        </div>
+        <div class="title">
+          AppleID
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <br>
+        <center>
+        <h1>AppleID</h1>
+       <img  style="margin-top: 2%; border-radius: 5%;" width="200" class="elevation-12 animated zoomIn" src="./img/appleid.png"><br>
+       <p>Email: 🔒🔒 </p>
+       <p>Pass: 🔒🔒</p>
+
+                <div   style="font-weight: 600; font-family:myFirstFont" class="item-text">تـــــکـــایــــە لـــــە ئــایـــکـــڵاود داخـــڵــی مــەکــە
+</div>
+
+         </center>
+         </br>     
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ pages/new.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Server</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+ <a href="#" class="link back">
+          <i class="white-color f7-icons">arrow_left</i> Back  </a>
+        </div>
+        <div class="title">
+          Documentation Of Which Apps Are Available ?
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <br>
+        <center>
+      
+VideoStar++ 🔴
+<br>
+VideoFx Live 🔴
+<br>
+Esign 🔴
+<br>
+unc0ver 🔴
+<br>
+OdysseyJB🔴
+<br>
+PPSSPP🔴
+<br>
+ProCam🔴
+<br>
+Bolt🔴
+<br>
+TikTok LRD🔴
+<br>
+Pubg Global🔴
+<br>
+Gta-San🔴
+<br>
+ORG 2020 HACK🔴
+<br>
+8 Ball Poll🔴
+<br>
+Minecraft🔴
+<br>
+KineMaster++🔴
+<br>
+AppCake🔴
+<br>
+Picsart Pro🔴
+          <br>
+          
+         </center>
+         </br>     
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
